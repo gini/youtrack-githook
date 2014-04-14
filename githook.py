0b9c2432cf05125c50dfec3271be3b8606e07a86
Jochen Schalanda <j.schalanda@gini.net>
2014-04-14 15:26:38 +0200

    Add more information to comment on GitLab push event

--- githook.py
+++ githook.py
@@ -24,43 +24,49 @@ def ping():
     return 'ping'
 
 @app.route('/hook', methods=['POST'])
-def receive_hook():
-    payload = request.json
-    user_name = payload['user_name']
-    repo_url = payload['repository']['url']
-    app.logger.debug('Received payload for a push by %s on repository %s', user_name, repo_url)
+@app.route('/push_event', methods=['POST'])
+def push_event_hook():
+    push_event = request.json
+    app.logger.debug(push_event)
+    user_name = push_event['user_name']
+    repo_name = push_event['repository']['name']
+    repo_url = push_event['repository']['url']
+    repo_homepage = push_event['repository']['homepage']
+    refspec = push_event['ref']
+    app.logger.debug('Received push event by %s in branch %s on repository %s', user_name, refspec, repo_url)
 
-    for commit in payload['commits']:
+    for commit in push_event['commits']:
         app.logger.debug('Processing commit %s by %s (%s) in %s', commit['id'], commit['author']['name'], commit['author']['email'], commit['url'])
         commit_time = dateutil.parser.parse(commit['timestamp'])
-        refs = re.findall(app.config['REGEX'], commit['message'], re.MULTILINE)
-        if not refs:
-            app.logger.info('''Didn't find any referenced issues in commit %s''', commit['id'])
+        issues = re.findall(app.config['REGEX'], commit['message'], re.MULTILINE)
+        if not issues:
+            app.logger.debug('''Didn't find any referenced issues in commit %s''', commit['id'])
         else:
-            app.logger.info('Found %d referenced issues in commit %s', len(refs), commit['id'])
-
+            app.logger.debug('Found %d referenced issues in commit %s', len(issues), commit['id'])
             yt = Connection(app.config['YOUTRACK_URL'], app.config['YOUTRACK_USERNAME'], app.config['YOUTRACK_PASSWORD'])
 
-            user = app.config['DEFAULT_USER']
+            default_user = yt.getUser(app.config['DEFAULT_USER'])
+            user_login = default_user['login']
+
             users = yt.getUsers({ 'q': commit['author']['email'] })
             if not users:
                 app.logger.warn('''Couldn't find user with email address %s. Using default user.''', commit['author']['email'])
             elif len(users) > 1:
                 app.logger.warn('''Found more than one user with email address %s. Using default user.''', commit['author']['email'])
             else:
-                user = users[0]['login']
+                user_login = users[0]['login']
 
-            for ref in refs:
-                app.logger.info('Processing reference to issue %s', ref)
+            for issue_id in issues:
+                app.logger.debug('Processing reference to issue %s', issue_id)
                 try:
-                    issue = yt.getIssue(ref)
-                    comment_string = 'Commit [%(url)s %(id)s] made by %(author)s on %(date)s\n{quote}%(message)s{quote}' % {'url': commit['url'], 'id': commit['id'], 'author': commit['author']['name'], 'date': str(commit_time), 'message': commit['message']}
+                    issue = yt.getIssue(issue_id)
+                    comment_string = 'Commit [%(url)s %(id)s] on branch %(refspec)s in [%(repo_homepage)s %(repo_name)s] made by %(author)s on %(date)s\n{quote}%(message)s{quote}' % {'url': commit['url'], 'id': commit['id'], 'author': commit['author']['name'], 'date': str(commit_time), 'message': commit['message'], 'repo_homepage': repo_homepage, 'repo_name': repo_name, 'refspec': refspec}
                     app.logger.debug(comment_string)
-                    yt.executeCommand(issueId=ref, command='comment', comment=comment_string.encode('utf-8'),
-                                      run_as=user.encode('utf-8'))
+                    yt.executeCommand(issueId=issue_id, command='comment', comment=comment_string.encode('utf-8'),
+                                      run_as=user_login.encode('utf-8'))
                 except YouTrackException:
                     app.logger.warn('''Couldn't find issue %s''', ref)
-    return Response('Payload processed. Thanks!', mimetype='text/plain')
+    return Response('Push event processed. Thanks!', mimetype='text/plain')
 
 
 if __name__ == '__main__':
